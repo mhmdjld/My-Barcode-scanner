9a6fe8ac2e35415ba25b62ecc46753f71613ec5b
mhmdjld <150243321+mhmdjld@users.noreply.github.com>
2025-07-22 10:56:22 +0200

    - Name geändert

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'my-barcode-scanner',
+  appName: 'My Barcode Scanner',
   webDir: 'dist'
 };
 
